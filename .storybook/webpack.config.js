189be0b6126069b5a2d8d9b2403201e5d72db633
cleysonlb <cleysonwebdev@gmail.com>
2020-05-02 18:21:10 -0300

    configure eslint

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,6 +2,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" }
+      },
+      {
         test: /\.css$/,
         loader: 'style-loader'
       },
